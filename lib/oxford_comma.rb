7191793bb13723ce30e4c82faa4c667c6998d056
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-16 18:31:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,15 @@
 def oxford_comma(array)
-["kiwi"].join("") 
-["kiwi", "durian"].insert(1,"and").join(" ") 
-["kiwi,","durian,", "starfruit"].insert(2,"and").join(" ") 
-["kiwi,","durian,", "starfruit,", "mangos,", "dragon fruits"].insert(4,"and").join(" ") 
+if array <=1
+  return.join("") 
+  
+elsif array <=2
+return array.insert(1,"and").join(" ") 
+
+elsif array <=3
+return array.insert(2,"and").join(" ") 
+
+else array <=5
+  return array.insert(4,"and").join(" ") 
 ["kiwi,","durian,", "starfruit,", "mangos,", "dragon fruits,", "lychees,", "pomelos"].insert(6,"and").join(" ") 
 end
 
