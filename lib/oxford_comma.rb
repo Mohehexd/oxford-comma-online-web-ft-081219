58bb59e37e08aecc7dff3ad2468ba90f62f18f58
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-16 18:26:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,6 @@
 def oxford_comma(array)
-  if array.length <= 1 
-    return array.join
-    
-  elsif array.length == 2 
-    return array.join(" and ")
-    
-  elsif array.length == 3
-    array[-1].insert(0, "and ")
-    return array.join(", " )
-  end
-end
-end
 
-
+end
 
 #["kiwi"].join("") √
 #["kiwi", "durian"].insert(1,"and").join(" ") √
