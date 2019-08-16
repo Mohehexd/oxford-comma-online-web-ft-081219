57817ee592243cb86c05e2586477de611614f715
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-16 18:27:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
 def oxford_comma(array)
-["kiwi"].join("") √
-["kiwi", "durian"].insert(1,"and").join(" ") √
-["kiwi,","durian,", "starfruit"].insert(2,"and").join(" ") √
-["kiwi,","durian,", "starfruit,", "mangos,", "dragon fruits"].insert(4,"and").join(" ") √
-["kiwi,","durian,", "starfruit,", "mangos,", "dragon fruits,", "lychees,", "pomelos"].insert(6,"and").join(" ") √
-
-
+["kiwi"].join("") 
+["kiwi", "durian"].insert(1,"and").join(" ") 
+["kiwi,","durian,", "starfruit"].insert(2,"and").join(" ") 
+["kiwi,","durian,", "starfruit,", "mangos,", "dragon fruits"].insert(4,"and").join(" ") 
+["kiwi,","durian,", "starfruit,", "mangos,", "dragon fruits,", "lychees,", "pomelos"].insert(6,"and").join(" ") 
 end
 
 #["kiwi"].join("") √
