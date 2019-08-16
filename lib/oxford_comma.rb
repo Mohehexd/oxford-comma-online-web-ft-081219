49fc38514ab66d207b627b7dfc95a77d0bdb3632
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-16 18:18:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,7 +10,7 @@ end
 # Ex. "'[12','34','35','231'].join("', '")}'"
 #ary.insert(index, obj...)
 
-
+#["kiwi,","durian,", "starfruit,", "mangos,", "dragon fruits"].insert(4,"and").join(" ")
 #["kiwi,","durian,", "starfruit"].insert(2,"and").join(" ") √
 #["kiwi", "durian"].insert(1,"and").join(" ") √
 #["kiwi"].join("") √
